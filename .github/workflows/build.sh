778d2c8be15fc19a3fda22eeea6515c29d536fb5
Bartłomiej Długosz <dlugosz.bartlomiej@gmail.com>
2025-05-21 00:33:46 +0200

    Update build.sh

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,8 +2,11 @@
 
 ./setup.sh
 sed -i 's/git\.openwrt\.org\/project\/luci/github\.com\/openwrt\/luci/g' ./feeds.conf.default
-./scripts/feeds update luci
-./scripts/feeds install luci
+./scripts/feeds update -a
+./scripts/feeds install \
+  luci-base \
+  luci \
+  ip
 mv ./bin/luci-app-device-list ./package/
 make defconfig
 make package/luci-app-device-list/compile V=s -j$(nproc) BUILD_LOG=1
